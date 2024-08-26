aca527c4973bf9b4c02e3a32348d13a7b0f3e061
StephenBuchinskiy <stephenbuchinskiy@outlook.com>
2024-08-26 12:56:44 -0600

    Added groups to make code optimized

--- main.py
+++ main.py
@@ -14,7 +14,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    # Create player
+    # Create groups and add Player
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     # Game loop
@@ -23,9 +26,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
+
+
+
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # Set to 60 fps and update clock
